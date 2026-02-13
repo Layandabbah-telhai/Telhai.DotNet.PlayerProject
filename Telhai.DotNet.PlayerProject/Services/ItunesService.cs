0b2766c667c58a8521108a9236848e5e247b108a
Layandabbah-telhai <layandab@m.telhai.ac.il>
2026-02-13 15:00:58 +0200

    Add project files.

---
+++ Telhai.DotNet.PlayerProject/Services/ItunesService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Telhai.DotNet.PlayerProject.Models;
+
+namespace Telhai.DotNet.PlayerProject.Services
+{
+        /// <summary>
+    /// service calling itunes search api
+    /// </summary>
+    public class ItunesService
+    {
+        //init http client with prefix base address domain
+        private static readonly HttpClient _httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("https://itunes.apple.com/")
+        };
+
+        public async Task<ItunesTrackInfo?> SearchOneAsync(
+            string songTitle,
+            CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrWhiteSpace(songTitle))
+                return null;
+
+            //bulid the request url
+            string encodedTerm = Uri.EscapeDataString(songTitle);
+            string url = $"search?term={encodedTerm}&media=music&limit=1";
+
+            using HttpResponseMessage response =
+                await _httpClient.GetAsync(url, cancellationToken);
+
+            response.EnsureSuccessStatusCode();
+
+            // get the response content as string json
+            string json = await response.Content.ReadAsStringAsync(cancellationToken);
+
+            // deserialize the json to ItunesSearchResponse object
+            var data = JsonSerializer.Deserialize<ItunesSearchResponse>(
+                json,
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+
+            var item = data?.Results?.FirstOrDefault();
+            if (item == null)
+                return null;
+
+            return new ItunesTrackInfo
+            {
+                TrackName = item.TrackName,
+                ArtistName = item.ArtistName,
+                AlbumName = item.CollectionName,
+                ArtworkUrl = item.ArtworkUrl100
+            };
+        }
+    }
+
+}
